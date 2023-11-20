52715f20bb481f420dfd0375521fdc7db959a5b2
wanliqun <wanliqunn@163.com>
2023-11-20 18:24:57 +0800

    Add `ThrottleClient` for HTTP concurrency control

---
+++ fetcher/client.go
@@ -0,0 +1,46 @@
+package fetcher
+
+import (
+	"context"
+	"net/http"
+)
+
+// ThrottleClient is a throttled HTTP client that limits the number of concurrent requests to
+// avoid resource overload and rate limiting issues.
+type ThrottleClient struct {
+	// Parallelism is the number of max allowed concurrent requests.
+	// Default 0 with unlimited concurrencies.
+	Parallelism int
+
+	client http.Client
+	ch     chan struct{}
+}
+
+func NewThrottleClient(parallelism int, client http.Client) *ThrottleClient {
+	c := &ThrottleClient{
+		Parallelism: parallelism,
+		client:      client,
+	}
+
+	if parallelism > 0 {
+		c.ch = make(chan struct{}, parallelism)
+	}
+
+	return c
+}
+
+func (c *ThrottleClient) Do(ctx context.Context, req *http.Request) (*http.Response, error) {
+	if c.Parallelism > 0 {
+		select {
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		case c.ch <- struct{}{}:
+		}
+
+		defer func() {
+			<-c.ch
+		}()
+	}
+
+	return c.client.Do(req)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/wanliqun/web-fetcher
+
+go 1.21.0
