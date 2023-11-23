19e0a610364f5410ed26127a18c69581d01b86f7
wanliqun <wanliqunn@163.com>
2023-11-23 15:29:37 +0800

    Support `--mirror` flag for run script

--- run.sh
+++ run.sh
@@ -2,21 +2,25 @@
 
 # check if there are any arguments
 if [ $# -eq 0 ]; then
-  echo "Usage: ./run.sh [--build | --metadata] [urls]"
-  echo "Example: ./run.sh --build --metadata https://www.google.com https://www.bing.com"
+  echo "Usage: ./run.sh [--build | --metadata | --mirror] [urls]"
+  echo "Example: ./run.sh --build --metadata --mirror https://www.google.com https://www.autify.com"
   echo "The --build flag (optional) builds the Docker image (first time only)"
   echo "The --metadata flag (optional) enables printing metadata about fetched web page"
+  echo "The --mirror flag (optional) enables downloading linked assets, such as images, stylesheets, and scripts."
   exit 1
 fi
 
 # Extract arguments from the command line
 args=()
 metaArg=""
+mirrorArg=""
 buildDockerImage=false
 
 for arg in "$@"; do
   if [[ $arg == "--metadata" || $arg == "-a" ]]; then
     metaArg="$arg"
+  elif [[ $arg == "--mirror" || $arg == "-m" ]]; then
+    mirrorArg="$arg"
   elif [[ $arg == "--build" ]]; then
     buildDockerImage=true
   else
@@ -45,4 +49,4 @@ if $buildDockerImage;  then
 fi
 
 # Run the Docker container with the specified arguments
-docker run --rm -v ${PWD}:/app/output web-fetcher ${metaArg} "${args[@]}"
+docker run --rm -v ${PWD}:/app/output web-fetcher ${metaArg} ${mirrorArg} "${args[@]}"
